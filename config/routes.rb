ba72a71841be0f723373d22129d2b1f94648e639
Keisuke-Takagi <k19990410@gmail.com>
2019-05-16 11:58:04 +0900

    base completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  resources :tweets, only: [:new, :index]
+  resources :tweets, only: [:new, :index, :create, :destroy, :edit, :update]
 end
